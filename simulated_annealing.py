af054e77c5060e3c7f5f8dddf1af39ea685a0e8b
Eeo Jun <141bytes@gmail.com>
2018-10-24 22:00:22 +0100

    some minor refactoring

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -4,10 +4,8 @@ import random
 
 def neighbour(s):
     x = s[:]
-    i = j = 0
-    while i == j:
-        i = random.randint(0, len(x) - 1)
-        j = random.randint(0, len(x) - 1)
+    i = random.randint(0,   len(x)-2)
+    j = random.randint(i+1, len(x)-1)
     x[i:j] = reversed(x[i:j])
     return x
 
@@ -23,21 +21,22 @@ def P(e, next_e, temp):
     return math.exp((e - next_e) / temp)
 
 
-def find_tour(matrix):
+def anneal(matrix):
     n = len(matrix)
     s = list(range(n))
     random.shuffle(s)
 
-    T = 1.0
+    T = 1.0 + 0.5 * math.log10(n)
     T_min = 0.00001
-    alpha = 0.996
+    alpha = 0.99
     e = cost(s, matrix)
+    r = random.random
 
     while T > T_min:
         for _ in range(100):
             next_s = neighbour(s)
             next_e = cost(next_s, matrix)
-            if random.random() < P(e, next_e, T):
+            if r() < P(e, next_e, T):
                 s = next_s
                 e = next_e
         T *= alpha
