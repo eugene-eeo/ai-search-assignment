10c1dc2cf7bfa88c5e7d5865bdb6ce436bd01e45
Eeo Jun <141bytes@gmail.com>
2018-11-15 19:00:10 +0000

    time to grind

--- aco_test.py
+++ aco_test.py
@@ -6,33 +6,48 @@ h.update(open('aco/aco', 'rb').read())
 sig = h.hexdigest()[:10]
 
 
-small_configs = [
-    ['-beta=2', '-pg=0.9', '-rho=0.9', '-G=2000'],
-    ['-beta=3', '-pg=0.9', '-rho=0.9', '-G=2000'],
-    ['-beta=4', '-pg=0.9', '-rho=0.9', '-G=2000'],
-    ['-beta=2', '-pg=0.5', '-rho=0.9', '-G=2000'],
-    ['-beta=2', '-pg=0.6', '-rho=0.9', '-G=2000'],
-    ['-beta=2', '-pg=0.7', '-rho=0.9', '-G=2000'],
-    ['-beta=2', '-pg=0.9', '-rho=0.6', '-G=2000'],
-    ['-beta=2', '-pg=0.9', '-rho=0.7', '-G=2000'],
-    ['-beta=2', '-pg=0.9', '-rho=0.8', '-G=2000'],
-    ['-beta=2', '-pg=0.3', '-rho=0.6', '-G=2000'],
-    ['-beta=2', '-pg=0.4', '-rho=0.7', '-G=2000'],
-    ['-beta=2', '-pg=0.5', '-rho=0.8', '-G=2000'],
-]
-
-large_configs = [
-    ['-beta=2', '-pg=0.9', '-rho=0.4', '-G=300'],
-]
-
+configs = {
+    "012": [[]],
+    "017": [[]],
+    "021": [[]],
+    "026": [[]],
+    "042": [[]],
+    "048": [
+        ['-beta=3', '-pg=0.4', '-rho=0.9'],
+        ['-beta=3', '-pg=0.5', '-rho=0.9'],
+        ['-beta=3', '-pg=0.6', '-rho=0.9'],
+        ['-beta=3', '-pg=0.7', '-rho=0.9', '-G=5000'],
+        ['-beta=2', '-pg=0.7', '-rho=0.9', '-G=5000'], # best
+        ['-beta=1.5', '-pg=0.7', '-rho=0.9', '-G=5000'],
+        ['-beta=1', '-pg=0.7', '-rho=0.9', '-G=5000'],
+        ['-beta=3', '-pg=0.8', '-rho=0.9'],
+        ],
+    "058": [[]],
+    #"175": ['-beta=2', '-pg=0.9', '-rho=0.4', '-G=300'],
+    #"180": ['-beta=2', '-pg=0.9', '-rho=0.4', '-G=300'],
+    #"535": ['-beta=2', '-pg=0.9', '-rho=0.4', '-G=300'],
+}
 
 
 print(sig)
-for config in small_configs:
-    print(config)
-    for size in ['012', '017', '021', '026', '042', '048', '058']:
-        print('   ', size)
-        os.system('python read_file.py cityfiles/AISearchFile%s.txt | aco/aco' % (size,))
-    print()
-    print("------------------------------")
-    print()
+for size, configs in configs.items():
+    for config in configs:
+        file = "aco_results/aco-{hash}-{size}-{args}.txt".format(
+                hash=sig,
+                args=''.join(config),
+                size=size,
+                )
+        if os.path.exists(file):
+            print(" - " + size + " " + ' '.join(config or ['defaults']) + " (skip)")
+            continue
+        print(" - " + size + " " + ' '.join(config or ['defaults']))
+        cmd = (
+            'python read_file.py cityfiles/AISearchFile{size}.txt'
+            ' | aco/aco {args}'
+            ' 2> /dev/null > {file}'
+            )
+        os.system(cmd.format(
+            size=size,
+            args=' '.join(config),
+            file=file,
+            ))
